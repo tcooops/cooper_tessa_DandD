71afad73f9d5120d34027a9729d402e5a4cef87e
Tessa Benton-Cooper <tbc@Tessas-MacBook-Pro.local>
2020-03-07 15:55:49 -0500

    researching and fixing the reset. Fixing the drop zones to allow only one piece to be dropped at a time in its respective zone.

--- script/app.js
+++ script/app.js
@@ -1,29 +1,70 @@
 (() => {
 	// set up the puzzle pieces and boards
-	const navButtons = document.querySelectorAll("#buttonHolder img"),
-		puzzleBoard = document.querySelector('.puzzle-board'),
-		puzzlePiece = document.querySelectorAll('.puzzle-pieces img');
+	// navButtons -> images at the bottom of the page
 
-	//store the image names here 
-	// the [] signifies an array
+	const navButtons = document.querySelectorAll('#button-holder img'),
+	      puzzleBoard = document.querySelector('.puzzle-board'),
+		  dropZones = document.querySelectorAll('.drop-zone')
+		  puzzlePiece = document.querySelectorAll('.puzzle-pieces img');
+
+	// store images names here
 	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
+	// functions go here = what we want to have happen when our triggers fire
 	function changeImageSet() {
-		//change the thumbnail image on the left to match the button image
 
-		//index comes with a "forEach"... it's just part of it. You have to index your names
-		pieces.forEach((piece, index) => puzzlePiece[index].src=`images/${piece + this.dataset.puzzleindex}.jpg` )
+		// change the thumbnail images on the left to match the button image
+		// add an ID to the images
+		pieces.forEach((piece, id) => {
+			const puzzlePieces = document.querySelector(".puzzle-pieces")
+			puzzlePieces.appendChild(puzzlePiece[id])
+			puzzlePiece[id].src= `images/${piece + this.dataset.puzzleindex}.jpg`;
+			puzzlePiece[id].id= `${piece + this.dataset.puzzleindex}`;
+	});
 
-		//and set a background image on the drop zone container
-		//debugger;
-
-		puzzleBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleindex}.jpg)`
-		//aboive is referring to a variable inside a string itself. Thats the $ and curly brackets.
-		//the dollar sign is a "JavaScript Expression"
-		// do something, change the property, make something update on the page.
+		// and set a background image on the drop zone container
+		// debugger;
+		puzzleBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleindex}.jpg)`;
 	}
 
-// add some event heading for the new buttons
-navButtons.forEach(button => button.addEventListener('click', changeImageSet));
+	function dragStart(event) {
+		console.log('started a drag')
 
+		// capture the id of the element were dragging
+		// the dataTrandfer object is part of the drag event -> you can us this to temporarily store
+		// store data you can retrieve and use later
+		// like an audio track, as an example
+		event.dataTransfer.setData("text/plain", this.id);
+	}
+
+	function allowDrag(event) {
+		event.preventDefault();
+		console.log('you dragged something onto me!')
+	}
+
+	function allowDrop(event) {
+		console.log(event.target)
+		if (!this.hasChildNodes()) {
+
+			event.preventDefault();
+			console.log('you dropped something onto me!')
+
+		   	let currentPiece = event.dataTransfer.getData ("text/plain");
+		   	event.target.appendChild(document.querySelector(`#${currentPiece}`))
+		}
+
+	}
+
+	// add some event handling for the nav navButtons
+	navButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+	//set up drop and drag event on our puzzle pieces
+	puzzlePiece.forEach(piece => piece.addEventListener('dragstart', dragStart));
+
+	//set up the dragover event for our dropZones
+	dropZones.forEach(zone => zone.addEventListener('dragover', allowDrag));
+
+	dropZones.forEach(zone => zone.addEventListener('drop', allowDrop));
+
+	changeImageSet.call(navButtons[0]);
 })();
